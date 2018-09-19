0718ad1761f2242834ae5784ef9d03b8c7983a02
wainaina <wainainah@gmail.com>
2018-09-19 17:09:10 +0100

    cuc

--- PassionTea/src/test/java/com/qa/PassionTea/TestRunner.java
+++ PassionTea/src/test/java/com/qa/PassionTea/TestRunner.java
@@ -1,27 +1,31 @@
 package com.qa.PassionTea;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 import org.junit.runner.RunWith;
-import org.junit.runner.notification.Failure;
 
-import junit.framework.TestSuite;
+//import org.junit.runner.JUnitCore;
+//import org.junit.runner.Result;
+//import org.junit.runner.notification.Failure;
+//
+//import junit.framework.TestSuite;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = "classpath")
+@CucumberOptions(features = "rc/test/java/teaTesting")
 
 public class TestRunner {
 	
-	public static void main(String[] args) {
-		Result res = JUnitCore.runClasses(TestSuite.class);
-		
-		for (Failure fail : res.getFailures()) {
-			System.out.println(fail.toString());
-		}
-		
-		System.out.println(res.wasSuccessful());
-	}
+	
+	
+	
+//	public static void main(String[] args) {
+//		Result res = JUnitCore.runClasses(TestSuite.class);
+//		
+//		for (Failure fail : res.getFailures()) {
+//			System.out.println(fail.toString());
+//		}
+//		
+//		System.out.println(res.wasSuccessful());
+//	}
 
 }
